b73bd12ea0761f3b7b3cde1d9dd5e19b200d4bad
soonma <clsrn77990@naver.com>
2024-04-29 20:52:18 +0900

    #2/LvOneChapterFiveBySix

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fristnumber;
+        int secondnumber;
+        int result = 0;
+        int[] index = new int[10];
+        int chago=0;
+        String countinueYN;
+        int i = 0;
+
+        String operate;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("첫번째 숫자를 입력 하세요: ");
+            fristnumber = input.nextInt();
+            System.out.print("두번째 숫자를 입력 하세요: ");
+            secondnumber = input.nextInt();
+            System.out.print("연산자를 입력 하세요 ");
+            operate = input.next();
+
+//            5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
+//                    - 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
+//                    - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+//            연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정합니다.
+//            현재 저장된 index가 마지막(9)라면 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현합니다
+
+            if (operate.equals("+")) {
+                result = fristnumber + secondnumber;
+            } else if (operate.equals("-")) {
+                result = fristnumber - secondnumber;
+            } else if (operate.equals("*")) {
+                result = fristnumber * secondnumber;
+            } else if (operate.equals("/")) {
+                result = fristnumber / secondnumber;
+            } else if (operate.equals("%")) {
+                result = fristnumber % secondnumber;
+            }
+            System.out.println("결과: " + result);
+
+            chago = result;
+            //i가 10 이상 , 즉 배열의 입력 값을 초과 했을때 값을 밀어 내는 방식의 코드
+            if (i >= 10) {
+                for (int j = 0; j< index.length; j++  ){
+                    if (j == index.length-1) {
+                        index[j] = chago;
+                    } else {
+                        index[j] = index[j + 1];
+                    }
+                }
+            } else {
+                index[i] = result;
+            }
+            i++;
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : " + i + " 번째");
+
+            countinueYN = sc.nextLine();
+            if (countinueYN.equals("exit")) {
+                break;
+            }
+        }
+        for (int j = 0; j < index.length; j++) {
+            System.out.println(index[j]);
+        }
+    }
+}
+
+
+
+

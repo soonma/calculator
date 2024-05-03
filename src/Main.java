e027e0844ae16eb52616c1635760f64f718f98ac
soonma <clsrn77990@naver.com>
2024-05-03 11:09:00 +0900

    #양의 정수가 아닐떄 되돌아가는 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         while (true) {
 
             System.out.print("첫번째 숫자를 입력 하세요: ");
+
             fristnumber = input.nextInt();
             System.out.print("두번째 숫자를 입력 하세요: ");
             secondnumber = input.nextInt();

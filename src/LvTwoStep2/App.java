a704b1cac7d8ef37126ef54f98f6ba71a49b42f2
soonma <clsrn77990@naver.com>
2024-05-03 12:28:19 +0900

    #중복되는 try-catch문 삭제,연산식 ArithmeticCalculator로 이동

--- src/LvTwoStep2/App.java
+++ src/LvTwoStep2/App.java
@@ -1,6 +1,8 @@
 package LvTwoStep2;
 
 
+import LvTwo.BadOpertorException;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,117 +11,115 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         int fristnumber = 0;
-        int secondnumber =0;
-        int i = 0;
+        int secondnumber = 0;
+        int arrycnt = 0;
         char operate = 0;
+        ArithmeticCalculator calculator;
+        String scan = "";
 
-        String index = "";
-        
         while (true) {
             try {
                 System.out.print("사칙 연산을 진행 할거면 '사칙' 원의 넓이를 구하고 싶으면 '원넓이'를 입력 하세요 : ");
-                index = sc.nextLine();
-                if (!index.equals("사칙") && !index.equals("원넓이")) {
+                scan = sc.nextLine();
+
+                if (!scan.equals("사칙") && !scan.equals("원넓이")) {
                     throw new BadInputException();
-                } else {
-                    if (index.equals("사칙")) {
-                        ArithmeticCalculator calculator = new ArithmeticCalculator();
-                        calculator = new ArithmeticCalculator(new AddOperator(),new SubtractOperator(),new MultiplyOperator(),new DivideOperator(),new ModOperator());
-                        while (true) {
-                            try {
-                                System.out.print("첫번째 숫자를 입력 하세요: ");
-                                fristnumber = sc.nextInt();
-                                System.out.print("두번째 숫자를 입력 하세요: ");
-                                secondnumber = sc.nextInt();
-                                System.out.print("연산자를 입력 하세요 ");
-                                operate = sc.next().charAt(0);
-                                calculator.getCheck(operate);
-                            } catch (InputMismatchException e) {
-                                //Calculator 함수에서 오입력 시 발생하는 Exception 처리가 제대로 되지 않아 Main 함수에서 진행 아마 입력 하자마자 발생 해서 우선순위때문인듯
-                                System.out.println("값이 잘못 입력 했습니다, 다시해주세요");
-                                sc.nextLine(); //여기시 씹힙 현상 발생 하여 무한 루프 발생 해당 구문으로 초기화 진행
-                                calculator.failgetCheck();
-                            }
-                            System.out.println("이떄 체크값 "+calculator.setCheck());
-                            //exception 발생시 값이 저장되는 현상이 발생 하여 if문으로 참이면 값을 저장 ,삭제 여부 결정 거짓이면 스킵
-                            if ( calculator.setCheck()) {
-                                if (operate=='+'){
-                                    System.out.println("결과: " + calculator.addOperator.setResult(fristnumber,secondnumber));
-                                    calculator.setAddList();
-                                } else if (operate=='-'){
-                                    System.out.println("결과: " + calculator.subtractOperator.setResult(fristnumber,secondnumber));
-                                    calculator.setAddList();
-                                } else if (operate=='*'){
-                                    System.out.println("결과: " + calculator.multiplyOperator.setResult(fristnumber,secondnumber));
-                                    calculator.setAddList();
-                                } else if (operate=='/'){
-                                    System.out.println("결과: " + calculator.divideOperator.setResult(fristnumber,secondnumber));
-                                    calculator.setAddList();
-                                } else if(operate=='%'){
-                                    System.out.println("결과: " + calculator.modOperator.setResult(fristnumber,secondnumber));
-                                    calculator.setAddList();
-                                }
+                }
+                if (scan.equals("사칙")) {
+                    calculator = new ArithmeticCalculator(new AddOperator(), new SubtractOperator(), new MultiplyOperator(), new DivideOperator(), new ModOperator());
+                    while (true) {
 
+                        System.out.print("첫번째 숫자를 입력 하세요: ");
+                        fristnumber = Integer.parseInt(sc.nextLine());
+                        System.out.print("두번째 숫자를 입력 하세요: ");
+                        secondnumber = Integer.parseInt(sc.nextLine()); //씹힘 현상 수정하기 위해 형변환 시도, 덕분에 안씹히는거 확인
+                        System.out.print("연산자를 입력 하세요 ");
+                        operate = sc.next().charAt(0);
+                        calculator.getCheck(operate);
 
-                                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                                sc.nextLine(); // 여기서 Scanner 씹히는 현상이 발생 해서 한번 초기화 내컴퓨터만 그런걸수도..
-                                index = sc.nextLine();
-                                if (index.equals("remove")) {
-                                    calculator.setReMove();
-                                }
-
-                                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                                index = sc.nextLine();
-                                if (index.equals("inquiry")) {
-                                    calculator.setList();
-                                }
-
-                                i++;
-                                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : " + i + " 번째");
-                                index = sc.nextLine();
-                                if (index.equals("exit")) {
-                                    break;
-                                }
-                            }
+                        if (fristnumber < 0&& secondnumber < 0 ) {
+                            System.out.println("양의 정수가 아님");
+                            continue;
                         }
-                    } else if (index.equals("원넓이")) {
-                        CircleCalculator calculator = new CircleCalculator();
-                        while (true) {
-                            try {
-                                System.out.print("원의 반지름을 입력 하세요 : ");
-                                fristnumber = sc.nextInt();
-                            } catch (InputMismatchException e) {
-                                System.out.println("값이 잘못 입력 했습니다, 다시해주세요");
-                                calculator.setCheck();
-                                sc.nextLine();
-                            }
-                            if (calculator.getCheck()) {
-                                System.out.println("원의 넓이 : " + calculator.getCircleResult(fristnumber));
+
+
+
+                        System.out.println("이떄 체크값 " + calculator.setCheck());
+                        //exception 발생시 값이 저장되는 현상이 발생 하여 if문으로 참이면 값을 저장 ,삭제 여부 결정 거짓이면 스킵
+                        if (calculator.setCheck()) {
+                                System.out.println("결과: " + calculator.getResult(fristnumber, secondnumber,operate));
                                 calculator.setAddList();
-                                sc.nextLine();
 
-                                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                                index = sc.nextLine();
-                                if (index.equals("remove")) {
-                                    calculator.setReMove();
-                                }
+                            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
 
-                                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                                index = sc.nextLine();
-                                if (index.equals("inquiry")) {
-                                    calculator.setList();
-                                }
-                                i++;
-                                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : " + i + " 번째");
-                                index = sc.nextLine();
-                                if (index.equals("exit")) {
-                                    break;
-                                }
+                            scan = sc.nextLine();
+                            if (scan.equals("remove")) {
+                                calculator.setReMove();
+                            }
+
+                            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                            scan = sc.nextLine();
+                            if (scan.equals("inquiry")) {
+                                calculator.setList();
+                            }
+
+                            arrycnt++;
+                            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : " + arrycnt + " 번째");
+                            scan = sc.nextLine();
+                            if (scan.equals("exit")) {
+                                break;
                             }
                         }
                     }
                 }
+                if (scan.equals("원넓이")) {
+                    CircleCalculator Circlecal = new CircleCalculator();
+                    while (true) {
+
+                        System.out.print("원의 반지름을 입력 하세요 : ");
+                        fristnumber = Integer.parseInt(sc.nextLine());
+
+                        Circlecal.setCheck();
+
+                        if (Circlecal.getCheck()) {
+                            System.out.println("원의 넓이 : " + Circlecal.getCircleResult(fristnumber));
+                            Circlecal.setAddList();
+                            sc.nextLine();
+
+                            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                            scan = sc.nextLine();
+                            if (scan.equals("remove")) {
+                                Circlecal.setReMove();
+                            }
+
+                            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                            scan = sc.nextLine();
+                            if (scan.equals("inquiry")) {
+                                Circlecal.setList();
+                            }
+                            arrycnt++;
+                            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : " + arrycnt + " 번째");
+                            scan = sc.nextLine();
+                            if (scan.equals("exit")) {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("잘못 입력 했습니다. 확인 부탁드립니다.");
+
             } catch (BadInputException e) {
+                e.getMessage();
+
+            } catch (ArithmeticException e) {
+                System.out.println("두번 째 정수 값이 0 입니다. 확인해주세요");
+
+            } catch (BadOpertorException e) {
+                e.getMessage();
+                sc.nextLine();
+
             }
         }
     }
--- src/LvTwoStep2/ArithmeticCalculator.java
+++ src/LvTwoStep2/ArithmeticCalculator.java
@@ -8,7 +8,7 @@ public class ArithmeticCalculator extends Calculator {
     ArrayList<Double> numbers = new ArrayList<Double>();
 
     static double result;
-    private boolean check = true;
+    private boolean check = false;
 
     AddOperator addOperator;
     SubtractOperator subtractOperator;
@@ -30,18 +30,45 @@ public class ArithmeticCalculator extends Calculator {
         numbers.clear();
     }
 
-    public boolean getCheck(char operate) {
-        try {
+    public boolean getCheck(char operate) throws BadOpertorException {
             //연산식 이외에 문자가 들어오면 fail
             if (!(operate == ('+')) && !(operate == ('-')) && !(operate == ('*')) && !(operate == ('/'))) {
                 throw new BadOpertorException();
             }
             check = true;
-        } catch (BadOpertorException e) {
-            check = false;
-        }
-
         return check;
+    }
+
+    public double getResult(int fristnumber ,int secondnumber,char operate) throws BadOpertorException {
+        switch (operate) {
+            case '+':
+                result = addOperator.setResult(fristnumber,secondnumber);
+                check = true;
+                break;
+            case '-':
+                result = subtractOperator.setResult(fristnumber,secondnumber);
+                check = true;
+                break;
+            case '*':
+                result =multiplyOperator.setResult(fristnumber,secondnumber);
+                check = true;
+                break;
+            case '/':
+                if (secondnumber == 0) {
+                    throw new ArithmeticException();
+                }
+                result = divideOperator.setResult(fristnumber,secondnumber);
+                check = true;
+                break;
+            case '%':
+                result =modOperator.setResult(fristnumber,secondnumber);
+                check = true;
+                break;
+            default:
+                check = false;
+                throw new BadOpertorException();
+        }
+                return result;
     }
 
     public void setReMove() {
@@ -67,7 +94,6 @@ public class ArithmeticCalculator extends Calculator {
     }
 
     public boolean setCheck() {
-
         return check;
     }
     public void failgetCheck(){
